6a6f43ea1b28f31ed36d47ae7a9dd8303ff8fdc6
Muhammad Anis Uddin Nasir <Anis@muhammads-mbp.dhcp.hakozaki.ibm.com>
2017-03-16 14:43:54 +0900

    added code

--- src/main/java/kafkaProducer/Main.java
+++ src/main/java/kafkaProducer/Main.java
@@ -1,28 +1,64 @@
 package kafkaProducer;
 
+//import util.properties packages
 import java.util.Properties;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
+//import simple producer packages
 import org.apache.kafka.clients.producer.Producer;
+
+//import KafkaProducer packages
+import org.apache.kafka.clients.producer.KafkaProducer;
+
+//import ProducerRecord packages
 import org.apache.kafka.clients.producer.ProducerRecord;
 
-
+//Create java class named “SimpleProducer”
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String[] args) throws Exception{
+
+		// Check arguments length value
+		if(args.length == 0){
+			System.out.println("test");
+					return;
+		}
+
+		//Assign topicName to string variable
+		String topicName = args[0].toString();
+
+		// create instance for properties to access producer configs   
 		Properties props = new Properties();
-		props.put("bootstrap.servers", "localhost:4242");
-		props.put("acks", "all");
-		props.put("retries", 0);
-		props.put("batch.size", 16384);
-		props.put("linger.ms", 1);
-		props.put("buffer.memory", 33554432);
-		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-		Producer<String, String> producer = new KafkaProducer<>(props);
-		for(int i = 0; i < 100; i++)
-			producer.send(new ProducerRecord<String, String>("test", Integer.toString(i), Integer.toString(i)));
+		//Assign localhost id
+		props.put("bootstrap.servers", "localhost:9092");
 
-		producer.close();
+				//Set acknowledgements for producer requests.      
+				props.put("acks", "all");
+
+						//If the request fails, the producer can automatically retry,
+						props.put("retries", 0);
+
+				//Specify buffer size in config
+				props.put("batch.size", 16384);
+
+				//Reduce the no of requests less than 0   
+				props.put("linger.ms", 1);
+
+				//The buffer.memory controls the total amount of memory available to the producer for buffering.   
+				props.put("buffer.memory", 33554432);
+
+				props.put("key.serializer", 
+						"org.apache.kafka.common.serializa-tion.StringSerializer");
+
+				props.put("value.serializer", 
+						"org.apache.kafka.common.serializa-tion.StringSerializer");
+
+				Producer<String, String> producer = new KafkaProducer
+						<String, String>(props);
+
+				for(int i = 0; i < 10; i++)
+					producer.send(new ProducerRecord<String, String>(topicName, 
+							Integer.toString(i), Integer.toString(i)));
+				System.out.println("Message sent successfully");
+				producer.close();
 	}
 }
