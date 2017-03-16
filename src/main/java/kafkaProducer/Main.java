5bbe5ddf90c9de68ea44d731513b443abcdb638e
Muhammad Anis Uddin Nasir <Anis@muhammads-mbp.dhcp.hakozaki.ibm.com>
2017-03-16 16:52:05 +0900

    added code

--- src/main/java/kafkaProducer/Main.java
+++ src/main/java/kafkaProducer/Main.java
@@ -19,46 +19,29 @@ public class Main {
 		// Check arguments length value
 		if(args.length == 0){
 			System.out.println("test");
-					return;
+			return;
 		}
 
 		//Assign topicName to string variable
 		String topicName = args[0].toString();
 
-		// create instance for properties to access producer configs   
 		Properties props = new Properties();
+		props.put("bootstrap.servers", "localhost:4242");
+		props.put("acks", "all");
+		props.put("retries", 0);
+		props.put("batch.size", 16384);
+		props.put("linger.ms", 1);
+		props.put("buffer.memory", 33554432);
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-		//Assign localhost id
-		props.put("bootstrap.servers", "localhost:9092");
+		Producer<String, String> producer = new KafkaProducer
+				<String, String>(props);
 
-				//Set acknowledgements for producer requests.      
-				props.put("acks", "all");
-
-						//If the request fails, the producer can automatically retry,
-						props.put("retries", 0);
-
-				//Specify buffer size in config
-				props.put("batch.size", 16384);
-
-				//Reduce the no of requests less than 0   
-				props.put("linger.ms", 1);
-
-				//The buffer.memory controls the total amount of memory available to the producer for buffering.   
-				props.put("buffer.memory", 33554432);
-
-				props.put("key.serializer", 
-						"org.apache.kafka.common.serializa-tion.StringSerializer");
-
-				props.put("value.serializer", 
-						"org.apache.kafka.common.serializa-tion.StringSerializer");
-
-				Producer<String, String> producer = new KafkaProducer
-						<String, String>(props);
-
-				for(int i = 0; i < 10; i++)
-					producer.send(new ProducerRecord<String, String>(topicName, 
-							Integer.toString(i), Integer.toString(i)));
-				System.out.println("Message sent successfully");
-				producer.close();
+		for(int i = 0; i < 10; i++)
+			producer.send(new ProducerRecord<String, String>(topicName, 
+					Integer.toString(i), Integer.toString(i)));
+		System.out.println("Message sent successfully");
+		producer.close();
 	}
 }
